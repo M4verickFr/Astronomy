2986e183931a81cf1264d220253e36e16211b2b0
MaverickFR <maverick.perrollaz@pm.me>
2022-09-19 06:37:01 -0200

    simplify code

--- src/Astronomy/afficherImg2.py
+++ src/Astronomy/afficherImg2.py
@@ -19,34 +19,17 @@ img = np.array(data/255, dtype = np.uint8) #Converting float32 to uint8
 # Apply adaptive threshold with gaussian size 51x51
 thresh_gray = cv2.adaptiveThreshold(img, 255, adaptiveMethod=cv2.ADAPTIVE_THRESH_GAUSSIAN_C, thresholdType=cv2.THRESH_BINARY, blockSize=255, C=0)
 
-# Find connected components (clusters)
-nlabel,labels,stats,centroids = cv2.connectedComponentsWithStats(thresh_gray, connectivity=8)
-
-# Find second largest cluster (the cluster is the background):
-max_size = np.max(stats[1:, cv2.CC_STAT_AREA])
-max_size_idx = np.where(stats[:, cv2.CC_STAT_AREA] == max_size)[0][0]
-
-mask = np.zeros_like(thresh_gray)
-
-# Draw the cluster on mask
-mask[labels == max_size_idx] = 255
-
-# Use "open" morphological operation for removing some artifacts
-mask = cv2.morphologyEx(thresh_gray, cv2.MORPH_OPEN, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5,5)))
-
 # Find contours, and get the contour with maximum area
-cnts = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+cnts = cv2.findContours(thresh_gray, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 cnts = imutils.grab_contours(cnts)
 
 c = max(cnts, key=cv2.contourArea)
 
-ellipse = cv2.fitEllipse(c)
-cv2.ellipse(img, ellipse, (36,255,12), 2)
 
 # Draw contours with maximum size on new mask
-mask2 = np.zeros_like(mask)
+mask2 = np.zeros_like(thresh_gray)
+ellipse = cv2.fitEllipse(c)
 mask2=cv2.ellipse(mask2, ellipse, (36,255,12), -1)
-# cv2.drawContours(mask2, [c], -1, 255, -1)
 
 img[(mask2==0)] = 0
 
