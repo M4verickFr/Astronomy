040c174e4e8c6cd731523a61f8bbd77dad7d82d7
Célien KOEBERLÉ <celien.koeberle143@gmail.com>
2022-09-23 09:09:08 +0200

    Récupération de toutes les images du ciel

--- src/Astronomy/image.py
+++ src/Astronomy/image.py
@@ -1,11 +1,12 @@
 # coding: utf-8
-
+import numpy as np
 import sqlite3
 import matplotlib.pyplot as plt
 from astropy.utils.data import download_file
 from astropy.io import fits
 
-def coordinateToUrl():
+def coordinateToUrl(r, x):
+
     return None  
 
 def getImages(path):
@@ -19,5 +20,21 @@ def displayImage(image_file):
 
 if __name__ == "__main__":
     url = "https://archive.stsci.edu/cgi-bin/dss_search\?v\=3\&r\=13+39+53.2\&d\=-31+40+15.0\&h\=10.0\&w\=10.0\&f\=fits"
-    image_file = getImages("url")
+    for i in range(-900, 900, 2):
+        r = 0
+        x= i/10
+        step = 0.2 + 5.478128918342 * (10**-36) * np.exp(0.938859*np.abs(x))
+        if 77.5<x<87.5:
+            step+=(0.2*x-15.5)
+
+        while(r < 360):
+            # Récupérer l'image
+            coordinateToUrl(r, i)
+            image_file = getImages("url")
+
+            # Incrémentation
+            r += step
+    
+    
+    
     displayImage(image_file)
