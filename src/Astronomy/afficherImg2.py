9ae7d7c066f2c77b1f16ffb932e89fdc2ae5fb27
MaverickFR <maverick.perrollaz@pm.me>
2022-09-23 05:21:52 -0200

    remove mask

--- src/Astronomy/afficherImg2.py
+++ src/Astronomy/afficherImg2.py
@@ -6,9 +6,12 @@ from astropy.utils.data import download_file
 import numpy as np
 import imutils
 import cv2
+import skimage.exposure
+import operator
 
 
-fits_file = 'https://archive.stsci.edu/cgi-bin/dss_search?v=3&r=11+23.8&d=-02+06&h=10&w=10&f=fits'
+# fits_file = 'https://archive.stsci.edu/cgi-bin/dss_search?v=3&r=11+23.8&d=-02+06&h=10&w=10&f=fits'
+fits_file = 'https://archive.stsci.edu/cgi-bin/dss_search?v=3&r=13+39+53.2&d=-31+40+15.0&h=10.0&w=10.0&f=fits'
 image_file = download_file(fits_file, cache=True)
 hdu = fits.open(image_file)[0]
 wmap = WCS(hdu.header)
@@ -27,17 +30,13 @@ cnts = imutils.grab_contours(cnts)
 c = max(cnts, key=cv2.contourArea)
 
 # Draw contours with maximum size on new mask
-mask1 = np.zeros_like(thresh_gray)
 ellipse = cv2.fitEllipse(c)
-mask1=cv2.ellipse(mask1, ellipse, (36,255,12), -1)
-
-# Smooth mask
-kernel = np.ones((10, 10), 'uint8')
-mask2 = cv2.dilate(mask1, kernel, iterations=1)
-mask2 = cv2.distanceTransform(mask2, cv2.DIST_L2, 3)
+mask1 = np.zeros_like(thresh_gray)
+mask1=cv2.ellipse(mask1, ellipse, (255,255,255),-1)
 
 # apply mask
-data = data * mask2
+data[mask1==0] = 0
+# data[mask1<1] = 0
 
 # Extract region
 x,y,w,h = cv2.boundingRect(c)
