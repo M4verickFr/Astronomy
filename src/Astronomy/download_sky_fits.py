c1fbb261389d2c56df4f174cc2b0cf7897834595
Célien KOEBERLÉ <celien.koeberle143@gmail.com>
2022-11-08 09:24:38 +0100

    get supernovae fits
    get supernovae fits function download all supernovae fits based on supernovae_url.txt content

--- src/Astronomy/download_sky_fits.py
+++ src/Astronomy/download_sky_fits.py
@@ -8,8 +8,8 @@ from astropy.io import fits
 def coordinateToUrl(r, x):
     return f"https://archive.stsci.edu/cgi-bin/dss_search?v=3&r={r}&d={x}+0&h=12&w=12&f=fits"  
 
-def getImages(path):
-    image_file = download_file(path, cache=True)
+def download_fits(url):
+    image_file = download_file(url, cache=True)
     return image_file
 
 def displayImage(image_file):
@@ -17,8 +17,8 @@ def displayImage(image_file):
     plt.imshow(image_data, cmap='gray')
     plt.colorbar()
 
-def download_sky_fits():
-    all_images = []
+def get_sky_fits():
+    all_sky_fits = []
     drap=0
     for i in range(-900, 900, 2):
         print(f"********************** angle {i} **********************")
@@ -30,7 +30,7 @@ def download_sky_fits():
             # Récupérer l'image
             url = coordinateToUrl(r, x)
             print(url)
-            all_images.append(getImages(url))
+            all_sky_fits.append(download_fits(url))
             '''
             drap += 1
             if(drap > 5):
@@ -40,20 +40,25 @@ def download_sky_fits():
 
             # Incrémentation
             r += step
+    return all_sky_fits
+
+def get_supernovae_fits(path_url_txt):
+    f = open(path_url_txt, "r")
+    all_supernovae_fits = []
+    for url in f:
+        print(url)
+        all_supernovae_fits.append(download_fits(url))
+    return all_supernovae_fits
 
 
 if __name__ == "__main__":
-    download_sky_fits()
+    all_sky_fits = get_sky_fits()
     x = np.arange(0., 90., 0.2)
     plt.plot(x, 11.5/(90-np.abs(x)), 'b--')
     plt.plot([0, 45, 70, 80, 85, 89], [0.2, 0.282, 0.585, 1.2, 2.3, 11.5], 'ro')
     
     plt.show()
 
-    drap = 0
-    r = 0.01
-    x= 0
-    step = 11.5/(90-np.abs(x))
-      
+    all_supernovae_fits = get_supernovae_fits()
 
     # test()
