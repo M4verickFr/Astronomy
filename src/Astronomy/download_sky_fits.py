0efa973e969e201c67038cca5465044a10868ffe
Célien KOEBERLÉ <celien.koeberle143@gmail.com>
2022-11-08 08:53:43 +0100

    rename download_fits to download_sky_fits

--- src/Astronomy/download_sky_fits.py
+++ src/Astronomy/download_sky_fits.py
@@ -10,7 +10,6 @@ def coordinateToUrl(r, x):
 
 def getImages(path):
     image_file = download_file(path, cache=True)
-    print(image_file)
     return image_file
 
 def displayImage(image_file):
@@ -18,7 +17,7 @@ def displayImage(image_file):
     plt.imshow(image_data, cmap='gray')
     plt.colorbar()
 
-def astro():
+def download_sky_fits():
     all_images = []
     drap=0
     for i in range(-900, 900, 2):
@@ -42,44 +41,9 @@ def astro():
             # Incrémentation
             r += step
 
-    print(drap)
-    '''
-    fig = plt.figure(figsize = (16,16))
-
-    for i in range(len(all_images)):
-        plt.subplot(3,2,i+1)
-        displayImage(all_images[i])
-    plt.show()
-    '''
-
-
-def test():
-    from matplotlib.cm import get_cmap
-    import numpy as np
-
-    import pyvista as pv
-    from pyvista import examples
-
-    sphere = pv.Sphere(
-        radius=5, theta_resolution=179, phi_resolution=179, start_theta=270.001, end_theta=270
-    )
-
-    ellipsoid = pv.ParametricEllipsoid(xradius = 10, yradius=10, zradius = 10)
-
-    # Initialize the texture coordinates array
-    ellipsoid.active_t_coords = np.zeros((ellipsoid.points.shape[0], 2))
-
-
-
-    # Populate by manually calculating
-    
-
-    # And let's display it with a world map
-    tex = examples.load_globe_texture()
-    ellipsoid.plot(texture=tex)
 
 if __name__ == "__main__":
-    astro()
+    download_sky_fits()
     x = np.arange(0., 90., 0.2)
     plt.plot(x, 11.5/(90-np.abs(x)), 'b--')
     plt.plot([0, 45, 70, 80, 85, 89], [0.2, 0.282, 0.585, 1.2, 2.3, 11.5], 'ro')
@@ -90,23 +54,6 @@ if __name__ == "__main__":
     r = 0.01
     x= 0
     step = 11.5/(90-np.abs(x))
-        
-    while(r < 360):
-        # Récupérer l'image
-        url = coordinateToUrl(r, x)
-        print(url)
-        # all_images.append(getImages(url))
-        '''
-        drap += 1
-        if(drap > 5):
-            break
-        '''
-        drap += 1
-        if(r > 3):
-            break
-        # Incrémentation
-        r += step
-
-    print(drap)
+      
 
     # test()
