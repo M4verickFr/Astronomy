e5034cecf2f43c141da08b189154be5fcb9e7ad1
Célien KOEBERLÉ <celien.koeberle143@gmail.com>
2022-11-17 10:53:52 +0100

    Update download_fits.py

--- src/Astronomy/download_fits.py
+++ src/Astronomy/download_fits.py
@@ -2,14 +2,18 @@
 import numpy as np
 import sqlite3
 import matplotlib.pyplot as plt
-from astropy.utils.data import download_file
+from astropy.utils.data import download_file, download_files_in_parallel
 from astropy.io import fits
 
 def coordinateToUrl(r, x):
-    return f"https://archive.stsci.edu/cgi-bin/dss_search?v=3&r={r}&d={x}+0&h=12&w=12&f=fits"  
+    return f"https://archive.stsci.edu/cgi-bin/dss_search?v=3&r={r}&d={x}+0&h=10&w=10&f=fits"  
 
-def download_fits(url):
-    image_file = download_file(url, cache=True)
+def download_on_fits(url, location):
+    image_file = download_file(url, cache=True, pkgname=location)
+    return image_file
+
+def download_several_fits(urls, location):
+    image_file = download_files_in_parallel(urls, cache=True, pkgname=location)
     return image_file
 
 def displayImage(image_file):
@@ -25,12 +29,13 @@ def get_sky_fits():
         r = 0.01
         x= i/10
         step = 11.5/(90-np.abs(x))
-        
+        urls = []
         while(r < 360):
             # Récupérer l'image
             url = coordinateToUrl(r, x)
             print(url)
-            all_sky_fits.append(download_fits(url))
+            urls.append(url)
+            # all_sky_fits.append(download_fits(url, "sky"))
             '''
             drap += 1
             if(drap > 5):
@@ -40,25 +45,28 @@ def get_sky_fits():
 
             # Incrémentation
             r += step
+        t = download_files_in_parallel(urls, cache=True)
+        print(t)
+        all_sky_fits += t
     return all_sky_fits
 
 def get_supernovae_fits(path_url_txt):
     f = open(path_url_txt, "r")
-    all_supernovae_fits = []
+    urls = []
     for url in f:
-        print(url)
-        all_supernovae_fits.append(download_fits(url))
+        urls.append(url)
+    all_supernovae_fits = download_several_fits(f, "supernovae")
     return all_supernovae_fits
 
 
 if __name__ == "__main__":
-    all_sky_fits = get_sky_fits()
+    # all_sky_fits = get_sky_fits()
     x = np.arange(0., 90., 0.2)
     plt.plot(x, 11.5/(90-np.abs(x)), 'b--')
     plt.plot([0, 45, 70, 80, 85, 89], [0.2, 0.282, 0.585, 1.2, 2.3, 11.5], 'ro')
     
     plt.show()
 
-    all_supernovae_fits = get_supernovae_fits()
+    all_supernovae_fits = get_supernovae_fits("supernovae_url.txt")
 
     # test()
