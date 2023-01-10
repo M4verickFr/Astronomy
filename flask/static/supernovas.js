aec7a8ced207eaf6a4bdd6506eacc6d152f9c4e5
thezedoriane <doriane.theze@gmail.com>
2023-01-10 16:08:52 +0100

    Joli tableau des datas :)

--- flask/static/supernovas.js
+++ flask/static/supernovas.js
@@ -5,8 +5,40 @@ $.ajax({
     url: "/api/sn",
     type: "GET",
     dataType: "json",
-    sucess: data => {
-        console.log(data)
-        // TODO - create a boostrap table
+    success: data => {
+        //console.log(data)
+        for (let d of data){
+            let tr = document.createElement("tr")
+            tr.onclick = function(){
+                location.href="./viewer"
+            }
+            let name = document.createElement("td")
+            name.innerHTML = d.name
+            let galaxy = document.createElement("td")
+            galaxy.innerHTML = d.galaxy
+            let date = document.createElement("td")
+            date.innerHTML = d.date
+            let ra = document.createElement("td")
+            ra.innerHTML = d.ra
+            let decl = document.createElement("td")
+            decl.innerHTML = d.decl
+            let offset = document.createElement("td")
+            offset.innerHTML = d.offset
+            let mag = document.createElement("td")
+            mag.innerHTML = d.mag
+
+            tr.append(name)
+            tr.append(galaxy)
+            tr.append(date)
+            tr.append(ra)
+            tr.append(decl)
+            tr.append(offset)
+            tr.append(mag)
+
+            document.getElementById("table-supernovae").getElementsByTagName("tbody")[0].append(tr)
+
+            //$("#table-supernovae").DataTable()
+
+        }
     },
 })
